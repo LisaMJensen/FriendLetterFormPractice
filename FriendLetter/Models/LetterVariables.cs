42bb07363fcf48d23968a80b700cf1e0835d1e38
Lisa Jensen <leese_38@yahoo.com>
2019-07-29 14:00:12 -0700

    add practice form files

---
+++ FriendLetter/Models/LetterVariables.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FriendLetter.Models
+{
+    public class LetterVariable
+    {
+        public string Recipient { get; set; }
+        public string Sender { get; set; }
+
+    }
+}
---
+++ FriendLetter/Startup.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FriendLetter
+{
+    public class Startup
+    {
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+            name: "default",
+            template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
